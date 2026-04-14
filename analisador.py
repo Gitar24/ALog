7a74ba132301b2b3b320405d9cc481468ef38fd9
Gitar24 <arthurlopespassos159@gmail.com>
2026-04-14 17:56:49 -0300

    Refinando o analisador.py

--- analisador.py
+++ analisador.py
@@ -1,8 +1,16 @@
 import re
 def analisar(arquivo):
-  with open(arquivo,'r') as log:
-      for x in log:
-        resultado = re.search(r"(\w+\s\d+\s[\d:]+)\s(\w+)\s(.+?)\:(.+)", x)
-        if 'ERROR' in x or 'WARNING' in x:
-           if resultado:
-             print(f"\nData:{resultado.group(1)}\nApp:{resultado.group(3)}\nMensagem:{resultado.group(4)}")
+   padrao = r"(\w+\s+\d+\s+[\d:]+)\s(\w+)\s+(\w+\[\d+\])\:(.+?)\:(.+)"
+   with open(arquivo,'r') as log:
+      for linha in log:
+         linha = linha.strip()
+         if 'ERROR' in linha or 'WARNING' in linha:
+            resultado = re.search(padrao, linha)
+            if resultado:
+               print(f"\nData: {resultado.group(1).strip()}")
+               print(f"Host: {resultado.group(2).strip()}")
+               print(f"App: {resultado.group(3).strip()}")
+               print(f"Nivel: {resultado.group(4).strip()}")
+               print(f"Mensagem: {resultado.group(5).strip()}")
+            else:
+               print(f"\n linha imcompativel com o padrao: {linha.strip()}")
