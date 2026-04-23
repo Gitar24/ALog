b1e83e1cbc0b125a554d579e8ee25a2575c16c69
Gitar24 <arthurlopespassos159@gmail.com>
2026-04-22 21:47:59 -0300

    criei o reporter.py

---
+++ output/reporter.py
@@ -0,0 +1,6 @@
+def exibir(entrada):
+    mensagem = entrada.get("MESSAGE")
+    prioridade = entrada.get("PRIORITY")
+    service = entrada.get("_SYSTEMD_UNIT")
+    print(f"Mensagem: {mensagem}\nPrioridade: {prioridade}\nApp: {service}")
+exibir(entrada)
